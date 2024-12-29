2eac0886a2c9698b6c9dc7f063ea58ceb0acc188
Toufiqur Rahman <toufiq.dev@gmail.com>
2024-12-30 01:13:43 +0600

    fix: swagger route as per the email

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('/api-docs', app, document);
 
   app.useLogger(logger);
 
